6a31679f3b47b24554291b67f4bc089c19df0538
shraddhamahe <60483403+shraddhamahe@users.noreply.github.com>
2020-02-01 12:22:08 +0000

    Update ExpenseServiceImpl.java

--- src/main/java/com/expense/service/impl/ExpenseServiceImpl.java
+++ src/main/java/com/expense/service/impl/ExpenseServiceImpl.java
@@ -1,64 +1,72 @@
-package com.expense.service.impl;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.logging.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import com.expense.controller.ExpenseController;
-import com.expense.entity.Expense;
-import com.expense.entity.User;
-import com.expense.entity.repository.ExpenseRepository;
-import com.expense.entity.repository.UserRepository;
-import com.expense.service.ExpenseService;
-
-// Service implementation with business logic and access repository
-
-@Service
-public class ExpenseServiceImpl implements ExpenseService {
-
-	Logger log = Logger.getLogger(ExpenseController.class.getName());
-
-	@Autowired
-	private ExpenseRepository expenseRepository;
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Override
-	public Expense saveExpense(Expense expense) {
-		
-		//Buggy Code(ExpenseServiceTest.saveExpenseTest())
-		//expense.setAmount(2000.00);
-		//return expense;
-		
-		//Correct Code
-		return expenseRepository.save(expense);
+	package com.expense.service;
+	
+	import static org.junit.Assert.assertEquals;
+	import static org.hamcrest.CoreMatchers.*;
+	import static org.hamcrest.MatcherAssert.assertThat;
+	import static org.mockito.Mockito.when;
+	import java.util.ArrayList;
+	import java.util.List;
+	import java.util.logging.Logger;
+	
+	import org.junit.Before;
+	import org.junit.Ignore;
+	import org.junit.Test;
+	import org.mockito.InjectMocks;
+	import org.mockito.Mock;
+	import org.mockito.Mockito;
+	import org.mockito.MockitoAnnotations;
+	
+	import com.expense.controller.ExpenseController;
+	import com.expense.entity.Expense;
+	import com.expense.entity.User;
+	import com.expense.entity.repository.ExpenseRepository;
+	import com.expense.service.impl.ExpenseServiceImpl;
+	
+	public class ExpenseServiceTest {
+	
+	       Logger log = Logger.getLogger(ExpenseServiceTest.class.getName());
+	       
+	       @Mock
+	       ExpenseRepository expenseRepository;
+	
+	       @InjectMocks
+	       ExpenseService expenseService = new ExpenseServiceImpl();
+	
+	       @Before
+	       public void init() {
+	              MockitoAnnotations.initMocks(this);
+	       }
+	
+	       @Test
+	       public void saveExpenseTest() {
+	              Expense expense = new Expense();
+	              expense.setAmount(1000.00);
+	
+	              when(expenseRepository.save(Mockito.any(Expense.class))).thenReturn(expense);
+	              Expense created = expenseService.saveExpense(expense);
+	
+	              log.info("created: "+created);
+	              assertThat(created.getAmount(), is(1000.00));
+	       }
+	
+	       @Test
+	       public void getAllUserTest() {
+	
+	              Expense expense = new Expense();
+	              expense.setAmount(1000.00);
+	
+	              when(expenseRepository.save(Mockito.any(Expense.class))).thenReturn(expense);
+	              Expense created = expenseService.saveExpense(expense);
+	
+	              ArrayList<Expense> expenseList = new ArrayList<Expense>();
+	              expenseList.add(created);
+	              when(expenseRepository.findAll()).thenReturn(expenseList);
+	
+	              List<Expense> expenseList1 = expenseService.getExpense();
+	              assertEquals(1, expenseList1.size());
+	       }
+	       
+	
+	
 	}
-
-	public List<Expense> getExpense() {
-		return expenseRepository.findAll();
-	}
-
-	@Override
-	public List<Expense> getExpenseByUserId(Long userId) {
-		User user = userRepository.getOne(userId);
-		return expenseRepository.findByUser(user);
-	}
-
-	@Override
-	public Expense getById(Long expenseId) {
-		return expenseRepository.getOne(expenseId);
-	}
-
-	@Override
-	public void deleteById(Long expenseId) {
-		//Buggy Code(ExpenseControllerTest.testDeleteExpense())
-		//log.info("Debug delete");
-		
-		//Correct Code
-		expenseRepository.deleteById(expenseId);
-		expenseRepository.flush();
-	}
-
-}
